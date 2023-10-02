601d8730cd9bba7a08ce63342a1efcad9622a06f
Arthur <arthurdidier10@gmail.com>
2023-10-02 17:42:17 -0600

    Tu mensaje de commit

--- contact-list.js
+++ contact-list.js
@@ -1,11 +1,33 @@
 // Crear una lista de contactos con datos predefinidos
 
-let contactos = {
-    {nombre: 'Daniel Gonzales'},
-    {nombre: 'Jose Murillo'},
-    { nombre: 'Arthur Sabillon' },
-    {nombre: 'Oscar Arias'}
-};
+let contactos = [
+    {
+    nombre: 'Daniel ',
+    apellido: 'Gonzales',
+    Direccion: '27 calle 8 avenida,San Pedro Sula, Cortes',
+    Telefono: '+504-85441020'},
+{
+    nombre: 'Jose',
+    apellido: 'Murillo',
+    Direccion: '27 calle 8 avenida,San Pedro Sula, Cortes',
+    Telefono: '+504-85441020'},
+{
+    nombre: 'Arthur',
+    apellido: 'Sabillon',
+    Direccion: '27 calle 8 avenida,San Pedro Sula, Cortes',
+    Telefono: '+504-85441020'},
+{
+    nombre: 'Oscar ',
+    apellido: 'Arias',
+    Direccion: '27 calle 8 avenida,San Pedro Sula, Cortes',
+        Telefono: '+504-85441020'
+    },
+    {
+    nombre: 'Carlos',
+    apellido: 'Martinez',
+    Direccion: '27 calle 8 avenida,San Pedro Sula, Cortes',
+    Telefono: '+504-85441020'}
+];
    
 
 // Función para añadir un nuevo contacto a la lista
@@ -26,8 +48,46 @@ function imprimirContactos() {
     contactos.forEach(contacto => console.log(contacto.nombre));
 }
 
+function añadirContacto(contacto) {
+    contactos.push(contacto);
+}
+
+function imprimirContactos() {
+    for(let i = 0; i < contactos.length; i++) {
+        console.log(contactos[i]);
+    }
+}
+
+// Ejemplo de uso:
+añadirContacto({
+    nombre: 'Armando',
+    apellido: 'Paredes',
+    Direccion: 'San Antonio, Ceiba, Atlantida',
+    Telefono: '+504-83457810',
+},
+{
+    nombre: 'Luis',
+    apellido: 'Peralta',
+    Direccion: 'Santa Lucia, Comayagua, Honduras',
+    Telefono: '+504-83154175'
+    },
+{
+    nombre: 'sergio',
+    apellido: 'Hernandez',
+    Direccion: 'San Pedro Sula, Cortes, Honduras',
+    Telefono: '+504-83124895'
+});  // Añade un nuevo contacto
+
+//imprimirContactos(); // Imprime los contactos
+
 // Ejemplo de uso:
 añadirContacto('David Galeano');  // Añade un nuevo contacto
 imprimirContactos();  // Imprime los contactos
 borrarContacto('Amner Martinez');  // Borra un contacto
 imprimirContactos();  // Imprime los contactos de nuevo
+añadirContacto('Manuel Reina'); //Añade un nuevo contacto
+imprimirContactos(); // Imprime los Contact
+añadirContacto('Ariel Guardado');  // Añade un nuevo contacto
+imprimirContactos();  // Imprime los contactos
+añadirContacto('Alex Herrera');  // Añade un nuevo contacto
+imprimirContactos();  // Imprime los contactos
